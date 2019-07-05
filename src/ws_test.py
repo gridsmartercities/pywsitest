8eb1441a07c817add1d223240f4f289060ae5627
Sam Noble <sam.noble@gridsmartercities.com>
2019-07-05 09:38:55 +0100

    added with_query_parameters method and test

--- src/ws_test.py
+++ src/ws_test.py
@@ -1,5 +1,9 @@
 class WSTest:
-    uri = None
 
     def __init__(self, uri):
         self.uri = uri
+        self.query_parameters = {}
+
+    def with_query_parameter(self, key, value):
+        self.query_parameters[key] = value
+        return self
