61adf7ee8ed7736ca7d91b2b075fa7434be81a41
Sam Noble <sam.noble@gridsmartercities.com>
2019-07-05 10:40:18 +0100

    decoupled tests

--- tests/test_ws_test.py
+++ tests/test_ws_test.py
@@ -7,16 +7,19 @@ class WSTestTests(unittest.TestCase):
 
     def test_create_ws_test_with_uri(self):
         uri = "wss://example.com"
-        self.ws_tester = WSTest(uri)
+        ws_tester = WSTest(uri)
 
-        self.assertEqual(uri, self.ws_tester.uri)
+        self.assertEqual(uri, ws_tester.uri)
 
     def test_add_key_value_query_parameter(self):
+        uri = "wss://example.com"
+        ws_tester = WSTest(uri)
+
         key = "example"
         value = 123
 
-        self.ws_tester.with_query_parameter(key, value)
+        ws_tester.with_query_parameter(key, value)
 
-        self.assertEqual(value, self.ws_tester.query_parameters[key])
+        self.assertEqual(value, ws_tester.query_parameters[key])
 
         
