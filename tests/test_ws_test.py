72ec4ce8f7debd2012bc6b3b5acfc5da3864c43f
Sam Noble <sam.noble@gridsmartercities.com>
2019-07-05 09:38:55 +0100

    added with_query_parameters method and test

--- tests/test_ws_test.py
+++ tests/test_ws_test.py
@@ -4,8 +4,19 @@ from src.ws_test import WSTest
 
 class WSTestTests(unittest.TestCase):
 
+
     def test_create_ws_test_with_uri(self):
         uri = "wss://example.com"
-        ws_tester = WSTest(uri)
+        self.ws_tester = WSTest(uri)
 
-        self.assertEqual(uri, ws_tester.uri)
+        self.assertEqual(uri, self.ws_tester.uri)
+
+    def test_add_key_value_query_parameter(self):
+        key = "example"
+        value = 123
+
+        self.ws_tester.with_query_parameter(key, value)
+
+        self.assertEqual(value, self.ws_tester.query_parameters[key])
+
+        
