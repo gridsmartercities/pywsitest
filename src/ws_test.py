d3267a652cc8998e1d0e2c2c20955fa745180786
Sam Noble <sam.noble@gridsmartercities.com>
2019-07-08 10:56:41 +0100

    test and method for connecting to websockets

--- src/ws_test.py
+++ src/ws_test.py
@@ -1,3 +1,7 @@
+import asyncio
+import websockets
+import ssl
+
 class WSTest:
 
     def __init__(self, uri):
@@ -7,3 +11,7 @@ class WSTest:
     def with_query_parameter(self, key, value):
         self.query_parameters[key] = value
         return self
+
+    async def run(self):
+        async with(websockets.connect(self.uri, ssl=ssl.SSLContext())) as websocket:
+            pass
