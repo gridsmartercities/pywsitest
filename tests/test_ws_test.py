a5fccfeb9f818909ac725b11e6b0a8b42e9dd29a
Sam Noble <sam.noble@gridsmartercities.com>
2019-07-08 10:56:41 +0100

    test and method for connecting to websockets

--- tests/test_ws_test.py
+++ tests/test_ws_test.py
@@ -1,5 +1,7 @@
 import unittest
+from unittest.mock import patch, MagicMock
 from src.ws_test import WSTest
+import asyncio
 
 
 class WSTestTests(unittest.TestCase):
@@ -21,5 +23,16 @@ class WSTestTests(unittest.TestCase):
         ws_tester.with_query_parameter(key, value)
 
         self.assertEqual(value, ws_tester.query_parameters[key])
+
+    @patch("websockets.connect")
+    @patch("ssl.SSLContext")
+    async def test_websocket_connect(self, mock_ssl, mock_socket):
+        uri = "wss://example.com"
+        ws_tester = WSTest(uri)
+
+        await ws_tester.run()
+
+        mock_socket.assert_called_once_with((uri, mock_ssl))
+
 
         
