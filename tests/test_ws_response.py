3f6ce7a4d3c2d55c13529a60f1481cb54be9b938
Sam Noble <sam.noble@gridsmartercities.com>
2019-07-09 14:57:01 +0100

    is match method and test added

--- tests/test_ws_response.py
+++ tests/test_ws_response.py
@@ -1,6 +1,6 @@
 import unittest
-from unittest.mock import patch, MagicMock
 from src.ws_response import WSResponse
+
 
 class WSResponseTests(unittest.TestCase):
 
@@ -15,3 +15,57 @@ class WSResponseTests(unittest.TestCase):
 
         self.assertIn("test", ws_response.attributes)
         self.assertEqual(1, len(ws_response.attributes))
+
+    def test_with_attribute_value(self):
+        ws_response = WSResponse().with_attribute_value("test", 123)
+
+        self.assertEqual(123, ws_response.value_attributes["test"])
+        self.assertEqual(1, len(ws_response.value_attributes))
+
+    def test_all_is_match(self):
+        ws_response = (WSResponse()
+                       .with_attribute_value("type", "new_request")
+                       .with_attribute("body"))
+
+        test_data = {
+            "type": "new_request",
+            "body": {}
+        }
+
+        self.assertTrue(ws_response.is_match(test_data))
+
+    def test_attribute_value_is_match(self):
+        ws_response = (WSResponse()
+                       .with_attribute_value("type", "new_request")
+                       .with_attribute("body"))
+
+        test_data = {
+            "type": "new_request",
+            "not_body": {}
+        }
+
+        self.assertFalse(ws_response.is_match(test_data))
+
+    def test_attribute_is_match(self):
+        ws_response = (WSResponse()
+                       .with_attribute_value("type", "new_request")
+                       .with_attribute("body"))
+
+        test_data = {
+            "not_type": "new_request",
+            "body": {}
+        }
+
+        self.assertFalse(ws_response.is_match(test_data))
+
+    def test_none_is_match(self):
+        ws_response = (WSResponse()
+                       .with_attribute_value("type", "new_request")
+                       .with_attribute("body"))
+
+        test_data = {
+            "not_type": "new_request",
+            "not_body": {}
+        }
+
+        self.assertFalse(ws_response.is_match(test_data))
--- tests/test_ws_test.py
+++ tests/test_ws_test.py
@@ -14,28 +14,22 @@ def syncify(coro):
 class WSTestTests(unittest.TestCase):
 
     def test_create_ws_test_with_uri(self):
-        uri = "wss://example.com"
-        ws_tester = WSTest(uri)
-
-        self.assertEqual(uri, ws_tester.uri)
+        ws_tester = WSTest("wss://example.com")
+        self.assertEqual("wss://example.com", ws_tester.uri)
 
     def test_add_key_value_query_parameter(self):
-        uri = "wss://example.com"
-        ws_tester = WSTest(uri)
+        ws_tester = (
+            WSTest("wss://example.com")
+            .with_query_parameter("example", 123)
+        )
 
-        key = "example"
-        value = 123
-
-        ws_tester.with_query_parameter(key, value)
-
-        self.assertEqual(value, ws_tester.query_parameters[key])
+        self.assertEqual(123, ws_tester.query_parameters["example"])
 
     @patch("src.ws_test.websockets")
     @patch("ssl.SSLContext")
     @syncify
     async def test_websocket_connect(self, mock_ssl, mock_websockets):
-        uri = "wss://example.com"
-        ws_tester = WSTest(uri)
+        ws_tester = WSTest("wss://example.com")
 
         mock_socket = MagicMock()
         mock_socket.close = MagicMock(return_value=asyncio.Future())
@@ -49,16 +43,15 @@ class WSTestTests(unittest.TestCase):
 
         await ws_tester.run()
 
-        mock_websockets.connect.assert_called_once_with(uri, ssl=ssl_context)
+        mock_websockets.connect.assert_called_once_with("wss://example.com", ssl=ssl_context)
         mock_socket.close.assert_called_once()
 
     @patch("src.ws_test.websockets")
     @patch("ssl.SSLContext")
     @syncify
     async def test_websocket_connect_with_parameters(self, mock_ssl, mock_websockets):
-        uri = "wss://example.com"
         ws_tester = (
-            WSTest(uri)
+            WSTest("wss://example.com")
             .with_query_parameter("example", 123)
             .with_query_parameter("test", 456)
         )
@@ -73,9 +66,9 @@ class WSTestTests(unittest.TestCase):
         ssl_context = MagicMock()
         mock_ssl.return_value = ssl_context
 
-        await ws_tester.run()
-
         expected_uri = "wss://example.com?example=123&test=456"
+
+        await ws_tester.run()
 
         mock_websockets.connect.assert_called_once_with(expected_uri, ssl=ssl_context)
         mock_socket.close.assert_called_once()
