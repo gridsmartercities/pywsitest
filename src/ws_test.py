ae4702bd4ba74ea87134c6ab81714ea73ccf541c
Sam Noble <sam.noble@gridsmartercities.com>
2019-07-09 12:00:32 +0100

    No reference to self in with query parameter

--- src/ws_test.py
+++ src/ws_test.py
@@ -10,7 +10,6 @@ class WSTest:
 
     def with_query_parameter(self, key, value):
         self.query_parameters[key] = value
-        return self
 
     async def run(self):
         websocket = await websockets.connect(self._get_connection_string(), ssl=ssl.SSLContext())
