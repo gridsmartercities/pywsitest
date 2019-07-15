3410dac4832ee229848400d663018aed364abf39
Sam Noble <sam.noble@gridsmartercities.com>
2019-07-15 16:09:05 +0100

    refactored response attributes

--- src/ws_response.py
+++ src/ws_response.py
@@ -1,21 +1,16 @@
 class WSResponse:
 
     def __init__(self):
-        self.attributes = []
-        self.value_attributes = {}
+        self.attributes = {}
 
     def with_attribute(self, attribute, value=None):
-        if value:
-            self.value_attributes[attribute] = value
-        else:
-            self.attributes.append(attribute)
+        self.attributes[attribute] = value
         return self
 
     def is_match(self, response):
         for key in self.attributes:
             if key not in response:
                 return False
-        for key in self.value_attributes:
-            if response.get(key) != self.value_attributes[key]:
+            if self.attributes[key] is not None and response[key] != self.attributes[key]:
                 return False
         return True
