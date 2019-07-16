25a44e1a676ebad134e9d94e52eeddf417a52300
Rob Anderson <robert.anderson@gridsmartercities.com>
2019-07-16 15:40:37 +0100

    Added docstring to WSMessage

--- src/ws_message.py
+++ src/ws_message.py
@@ -2,13 +2,36 @@ import json
 
 
 class WSMessage:
+    """
+    A class representing a message to send through the websocket
+
+    ...
+
+    Attributes:
+        attributes (dict)
+
+    Methods:
+        with_attribute(key, value):
+            Adds an attribute and returns the WSMessage
+    """
 
     def __init__(self):
         self.attributes = {}
 
-    def with_attribute(self, key, value):
-        self.attributes[key] = value
-        return self
-
     def __str__(self):
         return json.dumps(self.attributes)
+
+    def with_attribute(self, key, value):
+        """
+        Adds a key/value pair to the attributes dictionary.
+        Returns the WSMessage instance.
+
+        Parameters:
+            key (obj): The key of the attribute
+            value (obj): The value of the attribute
+
+        Returns:
+            (WSMessage): The WSMessage instance with_attribute was called on
+        """
+        self.attributes[key] = value
+        return self
