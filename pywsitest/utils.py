74ae5d25776b8f1faeec445441e0b2dc30e8007c
jamsidedown <robertanderson1993@googlemail.com>
2020-03-03 23:41:43 +0000

    Updated docstrings and tidied up utils, refactored ws_message a little

--- pywsitest/utils.py
+++ pywsitest/utils.py
@@ -3,32 +3,19 @@ from typing import List, Tuple
 
 
 PATH_REGEX = re.compile(r"^\$\{(.*)\}$")
-ARRAY_REGEX = re.compile(r"^(.+?)\[(\d*)\](.*)$")
-
-WORD_REGEX = re.compile(r"^([^\[\]\/]+)((\/|\[\d*\]).*)?$")
-LIST_INDEX = re.compile(r"^\[(\d*)\](.*)$")
-
-
-def get_resolved_value(response: dict, path: str) -> object:
-    """
-    Retrieves a value from a dictionary at a given path
-
-    Parameters:
-        response (dict): The response to check against for a value
-        path (str): The path in the response to check for a value
-
-    Returns:
-        (object): The object at a given path, or None if the path can't be found
-    """
-    resolved = response
-    for part in path.split("/"):
-        if part not in resolved:
-            return None
-        resolved = resolved[part]
-    return resolved
 
 
 def get_resolved_values(response: [list, dict], path: str) -> List[object]:
+    """
+    Retrieves a list of values from a dictionary at a given path
+
+    Parameters:
+        response (list, dict): The response to check against for values
+        path (str): The path in the response to check for values
+
+    Returns:
+        (list[object]): The list of objects at a given path, empty if the path can't be found
+    """
     resolved = [response]
 
     for part in path.lstrip("/").split("/"):
