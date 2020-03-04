ec0ce975c95a57e8f8f351c62064c64618b06bae
jamsidedown <robertanderson1993@googlemail.com>
2020-03-04 09:46:38 +0000

    get_resolved_values only takes dicts as input

--- pywsitest/utils.py
+++ pywsitest/utils.py
@@ -5,12 +5,12 @@ from typing import List, Tuple
 PATH_REGEX = re.compile(r"^\$\{(.*)\}$")
 
 
-def get_resolved_values(response: [list, dict], path: str) -> List[object]:
+def get_resolved_values(response: dict, path: str) -> List[object]:
     """
     Retrieves a list of values from a dictionary at a given path
 
     Parameters:
-        response (list, dict): The response to check against for values
+        response (dict): The response to check against for values
         path (str): The path in the response to check for values
 
     Returns:
@@ -18,7 +18,7 @@ def get_resolved_values(response: [list, dict], path: str) -> List[object]:
     """
     resolved = [response]
 
-    for part in path.lstrip("/").split("/"):
+    for part in path.strip("/").split("/"):
         # iterate to count rather than over objects as objects can be updated in-place
         count = len(resolved)
         for i in range(count):
