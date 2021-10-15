54849d7b91571b9456879f4a13c25d6db3f048f1
Miriam <miriam.thomas@gridsmartercities.com>
2021-10-15 08:53:51 +0100

    trial with git tags

--- tools/git_tag_1.sh
+++ tools/git_tag_1.sh
@@ -2,9 +2,8 @@
 major_max=0;
 minor_max=0;
 version=$(awk -F'[ ="]+' '$1 == "version" { print $2 }' pyproject.toml)
+version=$(echo v$version)
 #echo $version
-git add -u
-git commit -m "${version}"
 git tag "${version}"
-git push -u origin "${version}"
+git push origin "${version}"
 exit 0
