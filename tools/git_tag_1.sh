6b24b6bf8b8f06e058c2594b928705d66e460b10
Miriam <miriam.thomas@gridsmartercities.com>
2021-10-19 09:18:49 +0100

    test git ref issue

--- tools/git_tag_1.sh
+++ tools/git_tag_1.sh
@@ -4,6 +4,12 @@ minor_max=0;
 version=$(awk -F'[ ="]+' '$1 == "version" { print $2 }' pyproject.toml)
 version=$(echo v$version)
 #echo $version
+GITHUB_REPO=pywsitest/tree/automate-test
 git tag "${version}"
-git push origin "${version}"
+git log origin/master
+echo $(git remote -v)
+git remote set-url origin https://$GITHUB_TOKEN@github.com/$GITHUB_REPO.git
+echo $(git remote -v)
+git push origin "${version}" --dry-run
+#git push origin "${version}" --dry run
 exit 0
