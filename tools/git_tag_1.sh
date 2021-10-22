d6f8a72994d16542c4d1488c79e50922aba9a0df
Miriam <miriam.thomas@gridsmartercities.com>
2021-10-22 10:30:12 +0100

    test pypi publish

--- tools/git_tag_1.sh
+++ tools/git_tag_1.sh
@@ -5,12 +5,12 @@ version=$(awk -F'[ ="]+' '$1 == "version" { print $2 }' pyproject.toml)
 version=$(echo v$version)
 #echo $version
 #GITHUB_REPO=pywsitest/tree/automate-test
-git checkout automate
+git checkout master
 git tag "${version}"
-git log origin/master
-echo $(git remote -v)
-git remote set-url origin "git@github.com:gridsmartercities/pywsitest.git"
-echo $(git remote -v)
-git push origin automate-test "${version}" --dry-run
+#git log origin/master
+#echo $(git remote -v)
+#git remote set-url origin "git@github.com:gridsmartercities/pywsitest.git"
+#echo $(git remote -v)
+git push origin master "${version}" --dry-run
 #git push origin "${version}" --dry run
 exit 0
