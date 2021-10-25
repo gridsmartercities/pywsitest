4b3090ed53197e7fab60dc1a8ada508ca2de218e
Miriam <miriam.thomas@gridsmartercities.com>
2021-10-25 13:18:26 +0100

    linting

--- tools/git_tag_1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-major_max=0;
-minor_max=0;
-version=$(awk -F'[ ="]+' '$1 == "version" { print $2 }' pyproject.toml)
-version=$(echo v$version)
-git checkout master
-git tag "${version}"
-git push origin master "${version}" --dry-run
-exit 0
